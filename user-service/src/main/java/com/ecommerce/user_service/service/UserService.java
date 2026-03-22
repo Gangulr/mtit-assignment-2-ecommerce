7557df4661889d2274ff9b09fa76b7a6109877b6
Gangulr <167494975+Gangulr@users.noreply.github.com>
2026-03-22 20:00:08 +0530

    latest changes

---
+++ user-service/src/main/java/com/ecommerce/user_service/service/UserService.java
@@ -0,0 +1,29 @@
+package com.ecommerce.user_service.service;
+
+import com.ecommerce.user_service.model.User;
+import com.ecommerce.user_service.repository.UserRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserService {
+
+    private final UserRepository repository;
+
+    public UserService(UserRepository repository) {
+        this.repository = repository;
+    }
+
+    public List<User> getAll() {
+        return repository.findAll();
+    }
+
+    public User save(User user) {
+        return repository.save(user);
+    }
+
+    public void delete(Long id) {
+        repository.deleteById(id);
+    }
+}
